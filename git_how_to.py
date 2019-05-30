99e0e9a51253fc0b22aac41dfcd935e5e9a9cd5f
huggaida <huggaida@gmail.com>
2019-05-30 23:52:01 +0600

    1231231231

---
+++ git_how_to.py
@@ -0,0 +1,10 @@
+git init
+git add (filename) .
+
+git commit -m "comment"
+
+git commit -a
+which will automatically notice any modified (but not new) files, add them to the index, and commit, all in one step.
+
+git pull
+git push
